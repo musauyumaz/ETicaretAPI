5aebbf4338326416a72cc80a48d6e4938d425eb3
musauyumaznotes <musauyumaznotes@gmail.com>
2022-10-19 14:19:21 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Configurations;
+using ETicaretAPI.Persistence.Contexts;
+using ETicaretAPI.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            
+
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
+            services.AddSingleton<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository,CustomerWriteRepository>();
+            services.AddSingleton<IOrderReadRepository,OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository,OrderWriteRepository>();
+            services.AddSingleton<IProductReadRepository,ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository,ProductWriteRepository>();
+        }
+    }
+}
