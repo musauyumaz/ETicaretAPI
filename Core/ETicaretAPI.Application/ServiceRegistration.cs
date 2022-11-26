6dd545cbdf4444d43a287951ea290db2f757d779
musauyumaznotes <musauyumaznotes@gmail.com>
2022-11-26 15:52:48 +0300

    Facebook Login İşlemleri

--- Core/ETicaretAPI.Application/ServiceRegistration.cs
+++ Core/ETicaretAPI.Application/ServiceRegistration.cs
@@ -1,10 +1,5 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application
 {
@@ -13,6 +8,7 @@ namespace ETicaretAPI.Application
         public static void AddApplicationServices(this IServiceCollection services) 
         {
             services.AddMediatR(typeof(ServiceRegistration));
+            services.AddHttpClient();
         }
     }
 }
