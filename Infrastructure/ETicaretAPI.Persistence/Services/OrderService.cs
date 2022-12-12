812d247f807cc88ddffd5fce98cf0721fc96273b
Musa UYUMAZ <113431738+musauyumaznotes@users.noreply.github.com>
2022-12-12 14:36:13 +0300

    Sipariş Detaylarının Kodlanması

--- Infrastructure/ETicaretAPI.Persistence/Services/OrderService.cs
+++ Infrastructure/ETicaretAPI.Persistence/Services/OrderService.cs
@@ -47,6 +47,7 @@ namespace ETicaretAPI.Persistence.Services
             {
                 Orders = await data.Select(o => new
                 {
+                    Id = o.Id,
                     CreatedDate = o.CreatedDate,
                     OrderCode = o.OrderCode,
                     TotalPrice = o.Basket.BasketItems.Sum(bi => bi.Product.Price * bi.Quantity),
@@ -56,6 +57,25 @@ namespace ETicaretAPI.Persistence.Services
             };
 
 
+        }
+
+        public async Task<SingleOrder> GetOrderByIdAsync(string id)
+        {
+            var data = await _orderReadRepository.Table
+                .Include(o => o.Basket)
+                    .ThenInclude(b => b.BasketItems)
+                        .ThenInclude(bi => bi.Product)
+                            .FirstOrDefaultAsync(o => o.Id == Guid.Parse(id));
+
+            return new()
+            {
+                Id = data.Id.ToString(),
+                BasketItems = data.Basket.BasketItems.Select(bi => new { bi.Product.Name, bi.Product.Price, bi.Quantity }),
+                Address = data.Address,
+                Description = data.Description,
+                CreatedDate = data.CreatedDate,
+                OrderCode = data.OrderCode
+            };
         }
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/OrdersController.cs
+++ Presentation/ETicaretAPI.API/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using ETicaretAPI.Application.Features.Commands.Order.CreateOrder;
 using ETicaretAPI.Application.Features.Queries.Order.GetAllOrder;
+using ETicaretAPI.Application.Features.Queries.Order.GetOrderById;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -26,9 +27,15 @@ namespace ETicaretAPI.API.Controllers
             return Ok(response);
         }
         [HttpGet]
-        public async Task<IActionResult> GetAllOrders([FromQuery]GetAllOrderQueryRequest getAllOrderQueryRequest)
+        public async Task<IActionResult> GetAllOrders([FromQuery] GetAllOrderQueryRequest getAllOrderQueryRequest)
         {
             GetAllOrderQueryResponse response = await _mediator.Send(getAllOrderQueryRequest);
+            return Ok(response);
+        }
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetOrderById([FromRoute] GetOrderByIdQueryRequest getOrderByIdQueryRequest)
+        {
+            GetOrderByIdQueryResponse response = await _mediator.Send(getOrderByIdQueryRequest);
             return Ok(response);
         }
     }
