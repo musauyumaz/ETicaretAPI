472d742ea9e3dd3d50350983494c35b92551d631
Musa UYUMAZ <113431738+musauyumaznotes@users.noreply.github.com>
2022-12-18 15:57:31 +0300

    Rol Yönetim Sayfasının Oluşturulması

---
+++ Core/ETicaretAPI.Application/Features/Queries/Role/GetRoleById/GetRoleByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using MediatR;
+
+namespace ETicaretAPI.Application.Features.Queries.Role.GetRoleById
+{
+    public class GetRoleByIdQueryHandler : IRequestHandler<GetRoleByIdQueryRequest, GetRoleByIdQueryResponse>
+    {
+        private readonly IRoleService _roleService;
+
+        public GetRoleByIdQueryHandler(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task<GetRoleByIdQueryResponse> Handle(GetRoleByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var data = await _roleService.GetRoleById(request.Id);
+            return new() { Id = data.id, Name = data.name };
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Controllers/ApplicationServicesController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ApplicationServicesController.cs
@@ -1,11 +1,14 @@
 ﻿using ETicaretAPI.Application.Abstractions.Services.Configurations;
-using Microsoft.AspNetCore.Http;
+using ETicaretAPI.Application.CustomAttributes;
+using ETicaretAPI.Application.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class ApplicationServicesController : ControllerBase
     {
         IApplicationService _applicationService;
@@ -16,6 +19,7 @@ namespace ETicaretAPI.API.Controllers
         }
 
         [HttpGet]
+        [AuthorizeDefinition(ActionType = ActionType.Reading, Definition ="Get Authorize Definition Endpoints",Menu ="Application Services")]
         public IActionResult GetAuthorizeDefinitionEndpoints()
         {
             var datas = _applicationService.GetAuthorizeDefinitionEndpoints(typeof(Program));
