0e1738b5d55ec9226e98de7d6abf4634f59b2cae
musauyumaz <musauyumaznotes@users.noreply.github.com>
2022-12-27 11:38:12 +0300

    QrCode Okuma ve Ürün Stock Bilgisi Güncelleme

--- Infrastructure/ETicaretAPI.Persistence/Services/ProductService.cs
+++ Infrastructure/ETicaretAPI.Persistence/Services/ProductService.cs
@@ -8,11 +8,13 @@ namespace ETicaretAPI.Persistence.Services
     public class ProductService : IProductService
     {
         private readonly IProductReadRepository _productReadRepository;
+        private readonly IProductWriteRepository _productWriteRepository;
         private readonly IQRCodeService _qRCodeService;
-        public ProductService(IProductReadRepository productReadRepository, IQRCodeService qRCodeService)
+        public ProductService(IProductReadRepository productReadRepository, IQRCodeService qRCodeService, IProductWriteRepository productWriteRepository)
         {
             _productReadRepository = productReadRepository;
             _qRCodeService = qRCodeService;
+            _productWriteRepository = productWriteRepository;
         }
 
         public async Task<byte[]> QrCodeToProductAsync(string productId)
@@ -21,7 +23,7 @@ namespace ETicaretAPI.Persistence.Services
             if (product == null)
                 throw new Exception("Product Not Found");
 
-            var plainObject = new 
+            var plainObject = new
             {
                 product.Id,
                 product.Name,
@@ -32,6 +34,16 @@ namespace ETicaretAPI.Persistence.Services
             string plainText = JsonSerializer.Serialize(plainObject);
 
             return _qRCodeService.GenerateQRCodeAsync(plainText);
+        }
+
+        public async Task StockUpdateToProductAsync(string productId, int stock)
+        {
+            Product? product = await _productReadRepository.GetByIdAsync(productId);
+            if (product == null)
+                throw new Exception("Product Not Found");
+
+            product.Stock = stock;
+            await _productWriteRepository.SaveAsync();
         }
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using ETicaretAPI.Application.Enums;
 using ETicaretAPI.Application.Features.Commands.Product.CreateProduct;
 using ETicaretAPI.Application.Features.Commands.Product.RemoveProduct;
 using ETicaretAPI.Application.Features.Commands.Product.UpdateProduct;
+using ETicaretAPI.Application.Features.Commands.Product.UpdateStockQrCodeToProduct;
 using ETicaretAPI.Application.Features.Commands.ProductImageFile.ChangeShowCaseImage;
 using ETicaretAPI.Application.Features.Commands.ProductImageFile.RemoveProductImage;
 using ETicaretAPI.Application.Features.Commands.ProductImageFile.UploadProductImage;
@@ -41,6 +42,12 @@ namespace ETicaretAPI.API.Controllers
         {
             var data = await _productService.QrCodeToProductAsync(productId);
             return File(data,"image/png");
+        }
+        [HttpPut("qrcode")]
+        public async Task<IActionResult> UpdateStockQrCodeToProduct([FromBody] UpdateStockQrCodeToProductCommandRequest updateStockQrCodeToProductCommandRequest)
+        {
+            UpdateStockQrCodeToProductCommandResponse response = await _mediator.Send(updateStockQrCodeToProductCommandRequest);
+            return Ok(response);
         }
         [HttpGet("{Id}")]
         public async Task<IActionResult> Get([FromRoute] GetByIdProductQueryRequest getByIdProductQueryRequest)
